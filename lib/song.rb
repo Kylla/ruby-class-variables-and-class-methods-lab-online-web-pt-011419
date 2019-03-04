749dc20462924bcc1f8ff5a1500f3d33e0fdb7ed
Kylla Harris <kylla.harris@gmail.com>
2019-03-04 17:24:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  @@genres = []
-  @@artists =[]
+  @@genres = [ ]
+  @@artists =[ ]
   
   def initialized(name, artist, genre)
     @name = name
